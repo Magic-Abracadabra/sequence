780e25eff292a2f794af82fd308fc36f56584423
Magic-Abracadabra <you2kai1yuan2@hainanu.edu.cn>
2025-07-18 11:32:24 +0800

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -1,4 +1,15 @@
 def seqseg(seq):
+	'''# Sequence
+ ## Obtaining sequence segments
+ Example:
+```python
+from sequence import seqseg
+print(seqseg([7,3,6,9,3]))
+```
+Output:
+```plaintext
+(([7, 3, 6, 9, 3],), ([7], [3, 6, 9, 3]), ([7, 3], [6, 9, 3]), ([7, 3, 6], [9, 3]), ([7, 3, 6, 9], [3]), ([7], [3], [6, 9, 3]), ([7], [3, 6], [9, 3]), ([7], [3, 6, 9], [3]), ([7, 3], [6], [9, 3]), ([7, 3], [6, 9], [3]), ([7, 3, 6], [9], [3]), ([7], [3], [6], [9, 3]), ([7], [3], [6, 9], [3]), ([7], [3, 6], [9], [3]), ([7, 3], [6], [9], [3]), ([7], [3], [6], [9], [3]))
+```'''
 	from itertools import combinations as c
 	subs = ()
 	n_elements = len(seq)
@@ -16,6 +27,22 @@ def seqseg(seq):
 	return subs
 
 def issublist(near, far):
+	'''# Sequence
+ ## Is it a sublist of this?
+ Example:
+```python
+from sequence import issublist
+if issublist([1,2], [1,2,3]):
+        print('Sublist')
+else:
+        print('No')
+```
+Output:
+```plaintext
+Sublist
+```
+
+ '''
 	i = len(near)
 	j = near[0]
 	if j not in far:
